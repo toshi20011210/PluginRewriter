49170b7faf4c39954ce21f65f18d36ac2c875eea
toshi20011210 <japan.toshi.taka@gmail.com>
2023-05-25 14:02:45 +0900

    first commit

---
+++ rewriter.py
@@ -0,0 +1,12 @@
+finalArray = []
+f = open('input.txt', 'r')
+inputString = f.read()
+tempArray = inputString.splitlines()
+for i in range(0, len(tempArray)):
+    splittedTempArray = tempArray[i].split(" ", 1)
+    finalArray.append(splittedTempArray[0])
+
+for i in range(0, len(finalArray)): 
+    if finalArray[i] != 'webdriver-manager' and finalArray[i] != 'selenium' and finalArray[i] != 'selenium-wire' and finalArray[i] != 'tk':
+        with open('output.txt', 'a') as f:
+            f.write('--exclude-module ' + finalArray[i] + '\n')
